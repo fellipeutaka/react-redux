b65771af46e4d291b0c82f5ebb8f610a84610009
fellipeutaka <fellipeutaka@gmail.com>
2022-04-27 11:41:07 -0300

    * improve(Sidebar): padding;

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -58,6 +58,7 @@ export default function Sidebar() {
                 onClick={() =>
                   dispatch(courseActions.toggleLesson({ module, lesson }))
                 }
+                p={4}
               >
                 <Text>{lesson.title}</Text>
               </AccordionPanel>
